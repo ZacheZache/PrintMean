e5f5a752517d912bf3b9576ccc8d9ef0ea58e85f
Zacharias Göransson <zacharias.goransson@student.kyh.se>
2020-09-10 13:37:11 +0200

    Revert "Deleted comments"
    This reverts commit 18168855a0f5994c1fd1dec6f4350b13c3580bd1.

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class PrintMean {
-
+    // Heltalsdivision
         public static void main(String[] args) {
             System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1, 3, 3));
             System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
@@ -9,7 +9,7 @@ public class PrintMean {
             System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1, -11, 24, 2));
         }
         public static String meanCalc(double a, double b, double c, double d){
-
+            // Gör en sträng av "mean"
             double mean = (a + b + c + d) / 4;
             String meanString = "is " + mean;
 
